5743446d6d17a3045883462549da8bd159a8af1b
crasgaitis <70042644+crasgaitis@users.noreply.github.com>
2022-08-06 22:11:11 -0700

    Updated imports

--- superhero.py
+++ superhero.py
@@ -2,12 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-from sklearn.impute import SimpleImputer
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 from PIL import Image
 
 st.title('If You Were a Superhero, Would You Be GOOD, NEUTRAL, or BAD?!?!')
@@ -20,49 +14,6 @@ def load_data(nrows):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
-
-from sklearn.base import BaseEstimator, TransformerMixin
-
-
-# Pipeline things
-class CustomRemover(BaseEstimator, TransformerMixin):
-
-    def __init__(self, useless_attribs):
-        self.useless_attribs = useless_attribs
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_copy = X.copy()
-
-        X_copy = X_copy.drop(self.useless_attribs, axis=1)
-
-        return X_copy  
-   
-   
-   from sklearn.impute import SimpleImputer
-
-useless_attribs = ["name", "Unnamed: 0"]
-num_attribs = ['Height', 'Weight']
-cat_attribs = ["Gender", "Eye color", "Race", "Hair color", "Publisher", "Skin color"]
-
-num_pipeline = Pipeline([
-  ("std_scaler", StandardScaler())
-])
-
-col_pipeline = ColumnTransformer([ 
-  ("num", num_pipeline, num_attribs), 
-  ("cat", OneHotEncoder(), cat_attribs),  
-])
-
-full_pipeline = Pipeline([   
-  ("remover", CustomRemover(useless_attribs)),  
-  ("col_pipeline", col_pipeline)
-])
-
-
-#end of pipeline things
 
 # Filter dataframe for top closest to user inputted data
 def closest(data, gender, eye_color, race, hair_color, height, publisher, skin_color, weight, top=3):
