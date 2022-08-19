4b37b79ceb2340e9ca9d70018c266b72c8a7b596
crasgaitis <crasgaitis@gmail.com>
2022-08-18 21:19:02 -0700

    consolidating utils

--- superhero.py
+++ superhero.py
@@ -6,8 +6,7 @@ from PIL import Image
 import pickle
 # import joblib
 import random
-from utils import closest
-from utils import CustomRemover
+from sklearn.base import BaseEstimator, TransformerMixin
 
 # loading
 
@@ -20,6 +19,52 @@ def load_data(file):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
+
+# Filter dataframe for top closest to user inputted data
+def closest(data, gender, eye_color, race, hair_color, height, publisher, skin_color, weight, top=3):
+    filtered = data.loc[(data["gender"] == gender)
+            & (data["eye color"] == eye_color)
+            & (data["race"] == race)
+            & (data["hair color"] == hair_color)
+            & (data["publisher"] == publisher)
+            & (data["skin color"] == skin_color)]
+    if len(filtered) < top:
+        filtered = data.loc[(data["gender"] == gender)
+            & (data["eye color"] == eye_color)
+            & (data["race"] == race)
+            & (data["hair color"] == hair_color)
+            & (data["publisher"] == publisher)]
+        if len(filtered) < top:
+            filtered = data.loc[(data["gender"] == gender)
+            & (data["eye color"] == eye_color)
+            & (data["race"] == race)
+            & (data["hair color"] == hair_color)]
+            if len(filtered) < top:
+                filtered = data.loc[(data["gender"] == gender)
+                & (data["eye color"] == eye_color)
+                & (data["race"] == race)]
+                if len(filtered) < top:
+                    filtered = data.loc[(data["gender"] == gender)
+                    & (data["eye color"] == eye_color)]
+                    if len(filtered) < top:
+                        filtered = data.loc[(data["gender"] == gender)]
+    return filtered
+
+class CustomRemover(BaseEstimator, TransformerMixin):
+
+    def __init__(self, useless_attribs):
+        self.useless_attribs = useless_attribs
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_copy = X.copy()
+
+        X_copy = X_copy.drop(self.useless_attribs, axis=1)
+
+        return X_copy
+
 
 # configuring streamlit
 
@@ -125,3 +170,6 @@ if submit:
     for match in matches:
         row_to_check = powers.loc[(powers["hero_names"] == match)]
         st.write(row_to_check)
+        
+        
+        
